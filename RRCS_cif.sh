14343d63d0073f29515b2ada09ebf687d10f5358
MuWang <18640057101@163.com>
2020-05-23 10:31:54 +0800

    Add files via upload

---
+++ RRCS_cif.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+# usage:RRCS_cif.sh 5kvm.pdb
+#
+# Please set scripts lib which contains RRSC.py cif2pdb.py to SCRIPTS_DIR
+SCRIPTS_DIR='/home/parric/Documents/github/RRCS/scripts_work_parric'
+
+RRSC_cif(){
+	
+	cif_f=$1
+	pdb_f="$cif_f".pdb
+	python3 $SCRIPTS_DIR/cif2pdb.py $cif_f
+	python3 $SCRIPTS_DIR/RRCS.py $pdb_f
+}
+
+
+RRSC_cif $1
